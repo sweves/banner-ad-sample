9788ef0d888b2ef3c7e2a87f70d8823bbd001cd8
Sarah Wever <sarahewever@gmail.com>
2018-11-05 20:21:21 -0500

    minify

--- client/config/postcss.config.js
+++ client/config/postcss.config.js
@@ -1,9 +1,11 @@
 // Plugin imports
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 // Config
 module.exports = {
   plugins: {
-    autoprefixer: { browsers: ['last 10 versions'] }
+    autoprefixer: { browsers: ['last 10 versions'] },
+    cssnano: {}
   },
 }
